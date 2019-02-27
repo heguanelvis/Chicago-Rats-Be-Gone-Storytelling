6c0ddf02b54578fdb89678f16b029f4ff2ba5833
heguanelvis <heguanelvis@gmail.com>
2019-02-26 19:47:04 -0600

    finally figured a solution to resizing slides

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,31 @@
 import "bootstrap-css-only";
 import "../css/style.css";
-import * as d3 from "d3";
+
 import ratfavi from "../img/ratfavi.png";
+
+import ScrollMagic from 'scrollmagic';
+import 'imports-loader?define=>false!scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap';
+import { TimelineMax } from "gsap";
+
+import * as d3 from "d3";
 import StepChart from "./charts/step";
 
 /* Images */
 let favicon = document.getElementById('favicon');
 favicon.href = ratfavi;
 
-/* Data & Plotting */
+/* Data */
 const files = ["data/chicago_5_year_complaints_by_date.json"];
 
+/* Plot */
 Promise.all(files.map(path => d3.json(path)))
     .then(res => {
         /* Data Naming */
         const complaintsFiveYear = res[0];
-      
+
         /* US Map*/
 
-        /* Step Chart */ 
+        /* Step Chart */
         const stepMargin = { left: 75, right: 75, top: 75, bottom: 75 }
         const stepWidth = 1000;
         const stepHeight = 700;
@@ -37,7 +44,7 @@ Promise.all(files.map(path => d3.json(path)))
         console.log(err);
     });
 
-/* Responsive Design */
+/* Responsive Control */
 function responsivefy(svg) {
     const container = d3.select(svg.node().parentNode),
         width = parseInt(svg.style("width")),
@@ -51,8 +58,24 @@ function responsivefy(svg) {
     d3.select(window).on(`resize.${container.attr("id")}`, resize);
 
     function resize() {
-        var targetWidth = parseInt(container.style("width"));
+        let targetWidth = parseInt(container.style("width"));
         svg.attr("width", targetWidth - 30);
         svg.attr("height", Math.round(targetWidth / aspect));
     }
 }
+
+/* Slider Control */
+let sliderController = new ScrollMagic.Controller();
+let wipeAnimation = new TimelineMax()
+    .fromTo(".story-section.us-map", 1, { x: "-100%" }, { x: "0%", ease: Linear.easeNone })
+    .fromTo(".story-section.us-map", 1, { y: "0%" }, { y: "-100%", ease: Linear.easeNone })
+    .fromTo(".story-section.step", 1, { x: "100%" }, { x: "0%", ease: Linear.easeNone })
+    .fromTo(".story-section.step", 1, { y: "0" }, { y: "-100%", ease: Linear.easeNone })
+    .fromTo(".story-section.donut", 1, { y: "-100%" }, { y: "0%", ease: Linear.easeNone })
+new ScrollMagic.Scene({
+    triggerElement: "#pinContainer",
+    triggerHook: "onLeave",
+    duration: "300%"
+}).setPin("#pinContainer")
+    .setTween(wipeAnimation)
+    .addTo(sliderController);
